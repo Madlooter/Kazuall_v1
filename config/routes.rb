fc1ab00822b83e6a596601d6f065ca781417fc05
Madlooter <morelle.antoine@gmail.com>
2020-06-12 16:51:54 +0200

    Hero link + devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  	devise_for :views
+	root 'static_pages#index'
+  	get 'static_pages/index'
+  	get 'static_pages/secret'
+  	devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20200609173253_change_table_name2.rb
+++
@@ -1,6 +0,0 @@
-class ChangeTableName2 < ActiveRecord::Migration[6.0]
-  def change
-  	rename_table :user_locations, :users_locations
-
-  end
-end
